e06f4fde6d47e5aec35b326ab1c2abaad00b6f2c
Lolly384 <PKA2711@mail.ru>
2024-04-09 15:14:55 +0600

    First commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import './Header.css'
+
+export default function Header() {
+    return (
+        <header className="App-header">
+            <h1><i>Name system</i></h1>
+            <div className='App-header-group'>
+                <input></input>
+                <p><strong>Settings</strong></p>
+                <p><strong>Log In</strong></p>
+            </div>
+        </header>
+    )
+}
---
+++ src/components/Now/Now.js
@@ -0,0 +1,30 @@
+import './Now.css'
+import moment from 'moment'; // Для современных браузеров
+import { useState, useEffect } from 'react'; // Добавлено useEffect для управления интервалом
+
+export default function Now({ contentType }) {
+    const [time, setTime] = useState(moment().format('HH:mm:ss')); // Инициализация текущего времени
+    const [date, setDate] = useState(moment().format('DD MMMM YYYY')); // Инициализация текущей даты
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTime(moment().format('HH:mm:ss')); // Обновление текущего времени
+            setDate(moment().format('DD MMMM YYYY')); // Обновление текущей даты
+        }, 1000); // Интервал обновления в миллисекундах (1000 миллисекунд = 1 секунда)
+
+        // Очистка интервала при размонтировании компонента
+        return () => clearInterval(intervalId);
+    }, []); // Пустой массив зависимостей означает, что useEffect будет вызван только после первого рендера
+
+    return (
+        <div className='App-main-contents-group'>
+            <div className='App-main-contents-group-date'>
+                <h1><strong>{contentType}</strong></h1>
+                <p>Today {date}</p>
+            </div>
+            <div className='App-main-contents-group-time'>
+                <p>{time}</p>
+            </div>
+        </div>
+    )
+}
